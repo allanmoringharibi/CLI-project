ee3558aa70eacbf631219a420afedd4c60fe4473
alexgharibi <ali52346@gmail.com>
2022-01-03 15:32:30 -0500

    add an error feedback in iframe for user

--- src/index.tsx
+++ src/index.tsx
@@ -48,7 +48,13 @@ const App = () => {
         <div id="root"></div>
         <script>
            window.addEventListener('message', (event) => {
+             try {
              eval(event.data);
+             } catch (err) {
+               const root = document.querySelector('#root');
+               root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
+               console.error(err);
+             }
            }, false);
         </script>
      </body>
