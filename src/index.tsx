c88046a062b82677934c8f079458c330c3dd4a77
alexgharibi <ali52346@gmail.com>
2022-01-03 13:17:50 -0500

    test and investigate iframe security issues

--- src/index.tsx
+++ src/index.tsx
@@ -37,6 +37,12 @@ const App = () => {
     });
 
     setCode(result.outputFiles[0].text);
+
+    try {
+      eval(result.outputFiles[0].text);
+    } catch (err) {
+      alert(err);
+    }
   };
 
   return (
@@ -49,8 +55,13 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
+      <iframe title="code-sandbox" sandbox="" srcDoc={html} />
     </div>
   );
 };
+
+const html = `
+<h1>Local Html doc</h1>
+`;
 
 ReactDOM.render(<App />, document.querySelector("#root"));
