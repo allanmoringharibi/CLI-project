01408efbcc8dd8003d0a38b7d7190f779ce65249
alexgharibi <ali52346@gmail.com>
2022-01-05 15:23:15 -0500

    refactor App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,12 @@
 import ReactDOM from "react-dom";
 import "bulmaswatch/superhero/bulmaswatch.min.css";
-import { useState } from "react";
-import CodeEditor from "./components/code-editor";
-import Perview from "./components/perview";
-import bundle from "./bundler";
+import CodeCell from "./components/code-cell";
 
 const App = () => {
-  const [code, setCode] = useState("");
-  const [input, setInput] = useState("");
-
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
-
   return (
     <div>
-      <CodeEditor
-        initialValue="const a = 1"
-        onChange={(value) => setInput(value)}
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <Perview code={code} />
+      <CodeCell />
+      <CodeCell />
     </div>
   );
 };
