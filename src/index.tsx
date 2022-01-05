63792b56a4e128ba1ee14e37f90da240840e3dd9
alexgharibi <ali52346@gmail.com>
2022-01-05 14:41:52 -0500

    refactor out a perview component

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,12 @@ import { useState, useEffect, useRef } from "react";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 import { fetchPlugin } from "./plugins/fetch-plugin";
 import CodeEditor from "./components/code-editor";
+import Preview from "./components/perview";
+import Perview from "./components/perview";
 
 const App = () => {
   const ref = useRef<any>();
-  const iframe = useRef<any>();
+  const [code, setCode] = useState("");
   const [input, setInput] = useState("");
 
   const startService = async () => {
@@ -27,8 +29,6 @@ const App = () => {
       return;
     }
 
-    iframe.current.srcdoc = html;
-
     const result = await ref.current.build({
       entryPoints: ["index.js"],
       bundle: true,
@@ -40,29 +40,8 @@ const App = () => {
       },
     });
 
-    // setCode(result.outputFiles[0].text);
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
+    setCode(result.outputFiles[0].text);
   };
-
-  const html = `
-  <html>
-     <head></head>
-     <body>
-        <div id="root"></div>
-        <script>
-           window.addEventListener('message', (event) => {
-             try {
-             eval(event.data);
-             } catch (err) {
-               const root = document.querySelector('#root');
-               root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
-               console.error(err);
-             }
-           }, false);
-        </script>
-     </body>
-  </html>
-  `;
 
   return (
     <div>
@@ -70,19 +49,10 @@ const App = () => {
         initialValue="const a = 1"
         onChange={(value) => setInput(value)}
       />
-      <textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <iframe
-        sandbox="allow-scripts"
-        srcDoc={html}
-        ref={iframe}
-        title="code-sandbox"
-      />
+      <Perview code={code} />
     </div>
   );
 };
