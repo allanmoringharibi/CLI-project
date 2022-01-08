8e787d37d445f90a706f3fbcdd5d1980b7693c42
alexgharibi <ali52346@gmail.com>
2022-01-08 08:49:01 -0500

    constrain vertical resize

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -7,7 +7,13 @@ interface ResizableProps {
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
   return (
-    <ResizableBox height={300} width={Infinity} resizeHandles={["s"]}>
+    <ResizableBox
+      height={300}
+      width={Infinity}
+      resizeHandles={["s"]}
+      maxConstraints={[Infinity, window.innerHeight * 0.9]}
+      minConstraints={[Infinity, 100]}
+    >
       {children}
     </ResizableBox>
   );
