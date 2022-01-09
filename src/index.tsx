3eaad88ba36ea286c75b5680b8d7a34a012fc8cb
alexgharibi <ali52346@gmail.com>
2022-01-09 14:55:02 -0500

    install MD Editor

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import ReactDOM from "react-dom";
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 import CodeCell from "./components/code-cell";
+import TextEditor from "./components/text-editor";
 
 const App = () => {
   return (
     <div>
-      <CodeCell />
+      <TextEditor />
       {/* <CodeCell /> */}
     </div>
   );
