1b8451d84105814b6d961d2ffeac0484c765c881
alexgharibi <ali52346@gmail.com>
2022-01-13 08:21:27 -0500

    action creator return type annotations

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import ReactDOM from "react-dom";
 import "bulmaswatch/superhero/bulmaswatch.min.css";
+import { Provider } from "react-redux";
+import { store } from "./state";
 import CodeCell from "./components/code-cell";
 import TextEditor from "./components/text-editor";
 
 const App = () => {
   return (
-    <div>
-      <TextEditor />
-      {/* <CodeCell /> */}
-    </div>
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+        {/* <CodeCell /> */}
+      </div>
+    </Provider>
   );
 };
 
