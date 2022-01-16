c365af8b2edf685de34848e6a060479eba21b5c7
alexgharibi <ali52346@gmail.com>
2022-01-16 12:46:20 -0500

    render the list cell

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import { Cell } from "../state/cell";
+import CellListItem from "./cell-list-item";
 
 interface CellsState {
   cells: {
@@ -13,12 +14,17 @@ interface CellsState {
 }
 
 const CellList: React.FC = () => {
-  useSelector<CellsState>(({ cells: { order, data } }) => {
+  const cells: any = useSelector<CellsState>(({ cells: { order, data } }) => {
     return order.map((id) => {
       return data[id];
     });
   });
-  return <div>cell</div>;
+
+  const renderedCells = cells.map((cell: any) => (
+    <CellListItem key={cell.id} cell={cell} />
+  ));
+
+  return <div>{renderedCells}</div>;
 };
 
 export default CellList;
