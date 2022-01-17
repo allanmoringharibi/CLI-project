ad9e50e1af61cd1748faacac9cab676ff162d1cc
alexgharibi <ali52346@gmail.com>
2022-01-17 11:03:09 -0500

    apply custom css to actionBar component

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -2,6 +2,7 @@ import { Cell } from "../state";
 import CodeCell from "./code-cell";
 import TextEditor from "./text-editor";
 import ActionBar from "./action-bar";
+import "./cell-list-item.css";
 
 interface CellListItemProps {
   cell: Cell;
@@ -10,17 +11,24 @@ interface CellListItemProps {
 const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
   let child: JSX.Element;
   if (cell.type === "code") {
-    child = <CodeCell cell={cell} />;
+    child = (
+      <>
+        <div className="action-bar-wrapper">
+          <ActionBar id={cell.id} />
+        </div>
+        <CodeCell cell={cell} />
+      </>
+    );
   } else {
-    child = <TextEditor cell={cell} />;
+    child = (
+      <>
+        <TextEditor cell={cell} />
+        <ActionBar id={cell.id} />
+      </>
+    );
   }
 
-  return (
-    <div>
-      <ActionBar id={cell.id} />
-      {child}
-    </div>
-  );
+  return <div className="cell-list-item">{child}</div>;
 };
 
 export default CellListItem;
