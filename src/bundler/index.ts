c33219156dd9f238b7e5d64d082ccbd26a5b9112
alexgharibi <ali52346@gmail.com>
2022-01-22 10:31:39 -0500

    Avoid naming conflict

--- src/bundler/index.ts
+++ src/bundler/index.ts
@@ -20,6 +20,8 @@ const bundle = async (rawcode: string) => {
         "process.env.NODE_ENV": '"production"',
         global: "window",
       },
+      jsxFactory: "_React.createElement",
+      jsxFragment: "_React.Fragment",
     });
     return { code: result.outputFiles[0].text, err: "" };
   } catch (err: any) {
--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -36,8 +36,21 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
 
     const cumulativeCode = [
       `
+      import _React from 'react';
+      import _ReactDOM from 'react-dom';
+
       const show = (value) => {
-          document.querySelector('#root').innerHTML = value;
+        const root = document.querySelector('#root')
+
+        if (typeof value === 'object') {
+          if (value.$$typeof && value.props) {
+            _ReactDOM.render(value, root );
+          } else {
+            root.innerHTML = JSON.stringify(value);
+          }
+        } else {
+          root.innerHTML = value;
+        }
       };
       `,
     ];
