6dc024549d179bbb649f5c2f2c9a4dfb813143c2
alexgharibi <ali52346@gmail.com>
2022-01-22 10:42:59 -0500

    Implementing showFunc

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -34,29 +34,36 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const { data, order } = state.cells;
     const orderedCells = order.map((id) => data[id]);
 
-    const cumulativeCode = [
-      `
-      import _React from 'react';
-      import _ReactDOM from 'react-dom';
+    const showFunc = `
+    import _React from 'react';
+    import _ReactDOM from 'react-dom';
 
-      const show = (value) => {
-        const root = document.querySelector('#root')
+    var show = (value) => {
+      const root = document.querySelector('#root')
 
-        if (typeof value === 'object') {
-          if (value.$$typeof && value.props) {
-            _ReactDOM.render(value, root );
-          } else {
-            root.innerHTML = JSON.stringify(value);
-          }
+      if (typeof value === 'object') {
+        if (value.$$typeof && value.props) {
+          _ReactDOM.render(value, root );
         } else {
-          root.innerHTML = value;
+          root.innerHTML = JSON.stringify(value);
         }
-      };
-      `,
-    ];
+      } else {
+        root.innerHTML = value;
+      }
+    };
+    `;
+
+    const showFuncNoop = "var show = () => {}";
+
+    const cumulativeCode = [];
 
     for (let c of orderedCells) {
       if (c.type === "code") {
+        if (c.id === cell.id) {
+          cumulativeCode.push(showFunc);
+        } else {
+          cumulativeCode.push(showFuncNoop);
+        }
         cumulativeCode.push(c.content);
       }
       if (c.id === cell.id) {
